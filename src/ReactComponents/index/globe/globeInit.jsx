80ef581d3ecdf086895c10490350fd4e7e1f9fa8
Aaron Skeels <aaronskeelsofficial@gmail.com>
2024-01-06 19:47:50 -0800

    Update globeInit.jsx

--- src/ReactComponents/index/globe/globeInit.jsx
+++ src/ReactComponents/index/globe/globeInit.jsx
@@ -21,17 +21,17 @@ export default function() {
             }
             var globe1 = new globe(vw / 2, 3*vh / 4, vw/12, -Math.PI / 180.0 * .25, -Math.PI / 180.0 * .25, 0, document.getElementById("globeDiv"), false);
             new globeWord('g1', 'I am Aaron Skeels', degreeToRadian(90), degreeToRadian(0), 0, 0, 0, globe1);
-            new globeWord('g2', 'MatLab', degreeToRadian(90), degreeToRadian(180), 0, 0, 0, globe1, "/Portfolio/src/img/projects/matlab_min.webp");
-            new globeWord('g3', 'Java', degreeToRadian(90), degreeToRadian(90), 0, 0, 0, globe1, "/Portfolio/src/img/projects/java_min.webp");
-            new globeWord('g4', 'MySQL', degreeToRadian(90), degreeToRadian(-90), 0, 0, 0, globe1, "/Portfolio/src/img/projects/mysql_min.webp");
-            new globeWord('g5', 'Node.js', degreeToRadian(135), degreeToRadian(0), 0, 0, 0, globe1, "/Portfolio/src/img/projects/nodejs_min.webp");
-            new globeWord('g6', 'Photoshop', degreeToRadian(135), degreeToRadian(120), 0, 0, 0, globe1, "/Portfolio/src/img/projects/photoshop_min.webp");
-            new globeWord('g11', 'RaspberryPi', degreeToRadian(135), degreeToRadian(-120), 0, 0, 0, globe1, "/Portfolio/src/img/projects/rpi_min.webp");
-            new globeWord('g7', 'Javascript', degreeToRadian(180), degreeToRadian(0), 0, 0, 0, globe1, "/Portfolio/src/img/projects/js_min.webp");
-            new globeWord('g8', 'Unreal Engine', degreeToRadian(45), degreeToRadian(45), 0, 0, 0, globe1, "/Portfolio/src/img/projects/unrealengine_min.webp");
-            new globeWord('g9', 'C++', degreeToRadian(45), degreeToRadian(165), 0, 0, 0, globe1, "/Portfolio/src/img/projects/cpp_min.webp");
-            new globeWord('g12', 'Arduino', degreeToRadian(45), degreeToRadian(-75), 0, 0, 0, globe1, "/Portfolio/src/img/projects/arduino_min.webp");
-            new globeWord('g10', 'Blender', degreeToRadian(0), degreeToRadian(0), 0, 0, 0, globe1, "/Portfolio/src/img/projects/blender_min.webp");
+            new globeWord('g2', 'MatLab', degreeToRadian(90), degreeToRadian(180), 0, 0, 0, globe1, "/src/img/projects/matlab_min.webp");
+            new globeWord('g3', 'Java', degreeToRadian(90), degreeToRadian(90), 0, 0, 0, globe1, "/src/img/projects/java_min.webp");
+            new globeWord('g4', 'MySQL', degreeToRadian(90), degreeToRadian(-90), 0, 0, 0, globe1, "/src/img/projects/mysql_min.webp");
+            new globeWord('g5', 'Node.js', degreeToRadian(135), degreeToRadian(0), 0, 0, 0, globe1, "/src/img/projects/nodejs_min.webp");
+            new globeWord('g6', 'Photoshop', degreeToRadian(135), degreeToRadian(120), 0, 0, 0, globe1, "/src/img/projects/photoshop_min.webp");
+            new globeWord('g11', 'RaspberryPi', degreeToRadian(135), degreeToRadian(-120), 0, 0, 0, globe1, "/src/img/projects/rpi_min.webp");
+            new globeWord('g7', 'Javascript', degreeToRadian(180), degreeToRadian(0), 0, 0, 0, globe1, "/src/img/projects/js_min.webp");
+            new globeWord('g8', 'Unreal Engine', degreeToRadian(45), degreeToRadian(45), 0, 0, 0, globe1, "/src/img/projects/unrealengine_min.webp");
+            new globeWord('g9', 'C++', degreeToRadian(45), degreeToRadian(165), 0, 0, 0, globe1, "/src/img/projects/cpp_min.webp");
+            new globeWord('g12', 'Arduino', degreeToRadian(45), degreeToRadian(-75), 0, 0, 0, globe1, "/src/img/projects/arduino_min.webp");
+            new globeWord('g10', 'Blender', degreeToRadian(0), degreeToRadian(0), 0, 0, 0, globe1, "/src/img/projects/blender_min.webp");
           }
         `}</script>
       </Helmet>
